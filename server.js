bd78bf62e2920a7d20dda7379e2a26331686fbdd
ases573 <ases573@gmail.com>
2017-03-11 20:42:08 +0530

    post and push Commit

--- server.js
+++ server.js
@@ -17,31 +17,75 @@ app.use(cors());
 app.get('/api/contacts',function(request,response){
     if(!contacts)
     {
-        response.status(404).json({message : "No contacts"});
+        response.status(404).json({message : "No contacts Found"});
     }
 
     response.json(contacts);
 });
 
-app.get('/api/contacts/:firstName',function(request,response){
+app.get('/api/contacts/:id',function(request,response){
 
-const requestName = request.params.firstName;
+const requestid = request.params.id;
 
         let contact = contacts.filter(function(contact){
 
-            return contact.firstName == requestName;
+            return contact.id == requestid;
 
-        })
+        });
 
-         if(!contacts)
+         if(!contact)
         {
-            response.status(404).json({message : "No contacts"});
+            response.status(404).json({message : "No contact found"});
         }
 
 
 response.json(contact[0]);
 
+})
+
+app.post('/api/contacts/',function(request,response){
+
+const contact = {
+    id : contacts.length+1,
+    firstName : request.body.firstName,
+    lastName : request.body.lastName,
+    email : request.body.email,
+    website : request.body.website
+}
+
+contacts.push(contact);
+
+response.json(contact);
+
 });
+
+app.put('/api/contacts/:id',function(request,response){
+
+const requestid = request.params.id;
+
+        let contact = contacts.filter(function(contact){
+
+            return contact.id == requestid;
+
+        });
+
+       const index = contacts.indexOf(contact);
+
+       const keys = Object.keys(request.body);
+
+       keys.forEach(function(key){
+           
+           contact[key]= request.body[key];
+       
+        });
+
+        contacts[index] = contact;
+
+        response.json(contacts[index]);
+
+});
+
+
 
 const hostName = 'localhost';
 
